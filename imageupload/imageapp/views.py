9ac0b41f0ac1177e3d025d98864169877d0978bc
szymon-zuk <szymon.p.zuk@gmail.com>
2023-09-17 15:36:24 +0200

    Added serializers and views for UserTier and Image models

--- imageupload/imageapp/views.py
+++ imageupload/imageapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .models import Image, UserTier
+from .serializers import ImageSerializer
 
-# Create your views here.
+
+class ImageListCreateView(generics.ListCreateAPIView):
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Image.objects.filter(user=user)
+
+
+class ImageRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Image.objects.filter(user=user)
