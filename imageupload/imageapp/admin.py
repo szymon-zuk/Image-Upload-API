8974a4a37bd1ff669f4ecf74728b4780c558c449
szymon-zuk <szymon.p.zuk@gmail.com>
2023-09-17 13:18:19 +0200

    Added models UserTier and Image, registered UserTier in Django admin

--- imageupload/imageapp/admin.py
+++ imageupload/imageapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserTier
 
-# Register your models here.
+
+@admin.register(UserTier)
+class UserTierAdmin(admin.ModelAdmin):
+    list_display = ("user", "tier")
